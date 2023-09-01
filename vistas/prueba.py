032f9d64a2a8e661604e15903a7cab2e3a56416e
josmardiaz92 <josmardiaz92@gmail.com>
2023-08-31 22:34:44 -0400

    funcionando

--- vistas/prueba.py
+++ vistas/prueba.py
@@ -5,33 +5,37 @@ cgitb.enable()
 import sys
 sys.path.append('c:\\xampp\\htdocs\\Github\\directorio')
 from py.clases import turno as tur
+from py.clases import divisa as div
 print('Content-type: text/html\n\n')
 
-t = tur.turno()
-turnos = t.listar()
+t=tur.turno()
+d=div.divisa()
+
+turnos=t.listar()
+divisas=d.listar()
 
 especialidades = {}
 
 for datos in turnos:
     for str_data in datos:
         listaTurnos = str_data.strip('()').split(',')
-        cod_tur, fky_doc, nom_doc, fky_esp, nom_esp, fky_con, nom_con, fky_dia, nom_dia, ent_tur, sal_tur, est_tur = listaTurnos
+        t.cod_tur,t.fky_doc,t.nom_doc,t.fky_esp,t.nom_esp,t.fky_con,t.nom_con,t.fky_dia,t.nom_dia,t.ent_tur,t.sal_tur,t.est_tur = listaTurnos
 
-        if est_tur == 'A':
-            entrada=ent_tur.strip('""')
-            salida=sal_tur.strip('""')
-            horario = f"{nom_dia} {entrada} a {salida}"
+        if t.est_tur == 'A':
+            entrada=t.ent_tur.strip('""')
+            salida=t.sal_tur.strip('""')
+            horario = f"{t.nom_dia} {entrada} a {salida}"
             
 
-            if nom_esp not in especialidades:
-                especialidades[nom_esp] = {}
+            if t.nom_esp not in especialidades:
+                especialidades[t.nom_esp] = {}
 
-            if nom_doc not in especialidades[nom_esp]:
-                especialidades[nom_esp][nom_doc] = []
+            if t.nom_doc not in especialidades[t.nom_esp]:
+                especialidades[t.nom_esp][t.nom_doc] = []
 
-            especialidades[nom_esp][nom_doc].append({
+            especialidades[t.nom_esp][t.nom_doc].append({
                 'horario': horario,
-                'nom_con': nom_con  # Agregamos el nombre del consultorio
+                't.nom_con': t.nom_con  # Agregamos el nombre del consultorio
             })
 
 print('''
@@ -49,11 +53,13 @@ print('''
 </head>
 <body class="bg-dark">
     <div class="container-fluid h-100" id="contenedor">
+        <div class="row">
+            <div class="col-9">
 ''')
 
 for especialidad, doctores in especialidades.items():
     print(f'''
-        <div id="{especialidad}" class="col-9 text-capitalize text-warning">
+        <div id="{especialidad}" class="text-capitalize text-warning">
             <div class="mt-5">
                 <h1>{especialidad}</h1>
             </div>
@@ -73,7 +79,7 @@ for especialidad, doctores in especialidades.items():
                 <p>{horario}</p>
         ''')
 
-        consultorio_nombre = horarios[0]['nom_con']  # Tomamos el nombre del consultorio del primer horario
+        consultorio_nombre = horarios[0]['t.nom_con']  # Tomamos el nombre del consultorio del primer horario
         print(f'''
                 </div>
                 <div class="col-12 col-lg-2 align-self-center">
@@ -86,8 +92,51 @@ for especialidad, doctores in especialidades.items():
     print('''
         </div>
     ''')
-
 print('''
+        </div>
+    ''')
+for datos in divisas:
+    for str_data in datos:
+        listaDivisas=str_data.strip('()').split(',')
+        d.cod_div,d.nom_div,d.val_div,d.fec_div,d.est_div=listaDivisas
+        valor=float(d.val_div)
+        valor=round(valor, 2)
+print(f""" 
+<div class="col-3 d-flex text-capitalize text-warning d-block">
+                <div class="row">
+                    <div class="col text-center">
+                        <div class="row">
+                            <div class="col" id="titulo">
+                                <h1>
+                                    hospital san antonio de táriba
+                                </h1>
+                            </div>
+                        </div>
+                        <div class="row mt-5">
+                            <div class="col">
+                                <h2>
+                                    Tasa BCV
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="row mt-5">
+                            <div class="col" id="tasa">
+                                <h2>
+                                    {valor if d.nom_div == 'USD' else ''}
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="row mt-5">
+                            <div class="col img-fluid" id="logoestatico">
+                                <img src="../imagenes/logovertical.png" class="img-fluid logoEstatico bordeRedondeado p-3"  >
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div> """)
+
+print('''</div>
     </div>
 </body>
 </html>
